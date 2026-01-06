ac4d858a71dbbd295ed73ed6918c52972d3d96a7
vi6yav <vvermakol@gmail.com>
2026-01-07 03:43:38 +0530

    Final version

--- content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getPageText") {
-        sendResponse({ text: document.body.innerText });
-    }
-    return true; 
-});
